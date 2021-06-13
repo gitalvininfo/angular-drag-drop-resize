87f18ba587eb44a6e931a77294c02fb16ceb913a
Alvin <yansonalvin29@gmail.com>
2021-06-13 17:13:32 +0800

    drag drop with resize

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    DragDropModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
